e2712d23cb5965164bf031f156984138160c7047
Shekinah Jubahib <shekinahjubahib19@gmail.com>
2026-04-28 08:38:56 +0800

    save

--- landing-page-1/src/App.jsx
+++ landing-page-1/src/App.jsx
@@ -1,3 +1,16 @@
+/** cd landing-page-1 **/
+/**  ..\node-v24.15.0-win-x64\npm.cmd install **/
+/**Run daily: ..\node-v24.15.0-win-x64\npm.cmd run dev 
+
+short cut:
+
+$env:Path += ";C:\xampp\htdocs\Portfolio\node-v24.15.0-win-x64"
+
+Now, for the rest of that session, you can just type:npm.cmd run dev
+
+..\node-v24.15.0-win-x64\npm.cmd install @tailwindcss/postcss postcss
+**/
+
 import React from 'react';
 
 function App() {
